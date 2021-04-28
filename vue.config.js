ea70a4758e551699f48da1ec069a2709e7012a8b
codingwith3dv <deveshvdddas@gmail.com>
2021-04-28 13:25:54 +0530

    😉

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
-module.exports = { 
-  pwa: {
-    name: "Ticky Tacky Toeky", 
-    workboxPluginMode: 'InjectManifest', 
-    workboxOptions: { 
-      swSrc: 'src/service-worker.js'
-    }, 
-    themeColor: '#cc13ff' 
-  },
+module.exports = {
+	pwa: {
+		name: "Ticky Tacky Toeky",
+		themeColor: "#cc13ff",
+		workboxPluginMode: "InjectManifest",
+		workboxOptions: {
+			swSrc: "./src/service-worker.js"
+		},
+		
+	},
 }
