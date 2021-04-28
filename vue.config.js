bc285639b2353a88bd4e54bd1f4886e855212a65
codingwith3dv <deveshvdddas@gmail.com>
2021-04-28 14:40:52 +0530

    Hosting done

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,5 @@ module.exports = {
 	pwa: {
 		name: "Ticky Tacky Toeky",
 		themeColor: "#cc13ff",
-		workboxPluginMode: "InjectManifest",
-		workboxOptions: {
-			swSrc: "./src/service-worker.js"
-		},
-		
-	},
+	}
 }
