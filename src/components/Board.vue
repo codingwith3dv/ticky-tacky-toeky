d2f7f6ddcb4d21e4f7e1cf01737c811da9bac927
codingwith3dv <deveshvdddas@gmail.com>
2021-04-28 19:04:43 +0530

    Game board done ✅

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,39 +1,52 @@
 <template>
   <div>
     <button @click = "reset()">Reset</button>
-    <div v-bind:style="{
-      display: 'grid', 
-      gridTemplateColumns: 'repeat(' + boardSize + ', 1fr)',
-      gridGap: '1vw'
-    }">
-      <button
-        class="boardColumn"
-        v-for="(val, index) in cellsArray"
-        :key="index"
-        @click="move(index)"
-        >
-        {{ val }}
-      </button>
+    <h2 v-if=winner>The winner is {{ winner }}</h2>
+    <h2 v-else>Current Player {{ currentPlayer }}</h2>
+    <div class="boardContainer">
+      <div v-bind:style="{
+        display: 'grid', 
+        width: '100%',
+        gridTemplateColumns: 'repeat(' + boardSize + ', 1fr)',
+        gridGap: '.5vw',
+        boxSizing: 'border-box' 
+      }">
+        <Cell
+          v-for="(val, index) in cellsArray"
+          :key="index"
+          @click="move(index)"
+          :sqval="val"
+          />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
   import Cell from '@/components/Cell.vue';
-  import { ref } from 'vue';
+  import WOL from '@/brain/gameLogics/winnerOrLosser.ts';
+  import { ref, computed } from 'vue';
   export default {
     components: {
       Cell, 
     },
     setup() {
       let boardSize = ref(3);
+      let xTurn = ref(true);
+      let whoIsWinner = new WOL();
+      let currentPlayer;
       
       let cellsArray = ref(
         Array(boardSize.value * boardSize.value).fill(null)
       );
       
+      let winner = computed(() => whoIsWinner.checkWinner(cellsArray.value));
+      currentPlayer = computed(() => (xTurn.value ? "X" : "O"))
+      
       const move = (index: number) => {
-        cellsArray.value[index] = 'O'
+        if(cellsArray.value[index] || winner.value) return
+        cellsArray.value[index] = (xTurn.value ? "X" : "O")
+        xTurn.value = !xTurn.value
       }
       const reset = () => {
         cellsArray.value = 
@@ -44,19 +57,23 @@
         cellsArray, 
         move, 
         reset, 
+        xTurn, 
+        winner, 
+        currentPlayer
       }
     }, 
   }
 </script>
 
 <style scoped>
-.board {
+.boardContainer {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 1vw;
+  place-items: center;
+  width: 100%;
 }
-.boardColumn {
-  min-width: 100%;
-  aspect-ratio: 1;
+@media (min-width: 720px) {
+  .boardContainer {
+    width: 50vw;
+  }
 }
 </style>
--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="sq">{{ sqval }}</button>
+  <button class="sq sq-def" v-if="!sqval">{{ sqval }}</button>
+  <button class="sq sq-x" v-else-if="sqval == 'X'">{{ sqval }}</button>
+  <button class="sq sq-o" v-else-if="sqval == 'O'">{{ sqval }}</button>
 </template>
 
 <script>
@@ -11,9 +13,33 @@
   }
 </script>
 
-<style>
+<style scoped>
   .sq {
-    width: 100px;
-    height: 100px;
+    min-width: 100%;
+    aspect-ratio: 1;
+    outline: none;
+    border: 1px solid black;
+    font-size: 3rem;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+    border-radius: 5px;
+  }
+  .sq-def {
+    background-color: #cc13ff;
+  }
+  .sq-def:hover {
+    background-color: #cc13ff88;
+  }
+  .sq-x {
+    background-color: #137cff;
+  }
+  .sq-x:hover {
+    background-color: #137cff88;
+  }
+  .sq-o {
+    background-color: #ac00ff;
+  }
+  .sq-o:hover {
+    background-color: #ac00ff88;
   }
 </style>
